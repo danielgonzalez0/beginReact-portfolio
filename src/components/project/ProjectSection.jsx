3b5ca51cbac2202f0d2dedc3750334a8096a71b5
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-04-18 19:28:10 +0200

    refactor (ProjectSection) : refactor fetch data from github with useReducer to handle the request state

--- src/components/project/ProjectSection.jsx
+++ src/components/project/ProjectSection.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useReducer } from 'react';
 import { SectionWrapper } from '../atom/SectionWrapper';
 import { Project } from './Project';
 import { getListOfUrlRepositoriesUrl } from '../../lib/api-url';
@@ -7,33 +7,84 @@ import { Loader } from '../atom/Loader/Loader';
 
 export const ProjectSection = () => {
 
-  const [repositories, setRepositories] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-  // GitHub Repository - Exercise
-  useEffect(() => {
-    fetch(getListOfUrlRepositoriesUrl(GITHUB_USERNAME))
-      .then((res) => res.json())
-      .then((repo) => {
-        if(repo.error) throw new Error("Error fetching repositories")
-        setRepositories(repo)
-      })
-      .catch((err) => setError(err))
-      .finally(() => {
-        console.log(repositories);
-        setTimeout(() =>setLoading(false),1500)});
-      
+  //définir mes actions 
+  const FETCH_REDUCER_ACTIONS = {
+    PENDING: "pending",
+    RESOLVED: "resolved",
+    REJECTED: "rejected",
+  }
+
+  const IDLE = {
+    isLoading: false,
+    data: null,
+    error: null,
+  }
+
+  //définir mon reducer cad comment je vais gérer mes états
+  const fetchReducer = (state, action) => {
+    switch (action.type) {
+      case FETCH_REDUCER_ACTIONS.PENDING:
+        return {
+          ...state,
+          isLoading: true,
+        };
+      case FETCH_REDUCER_ACTIONS.RESOLVED:
+        return {
+          ...state,
+          isLoading: false,
+          data: action.payload,
+        };
+
+      case FETCH_REDUCER_ACTIONS.REJECTED:
+        return {
+          ...state,
+          isLoading: false,
+          error: action.payload,
+        };
+      default:
+        return new Error(`Invalid action type: ${action.type}`)
+    }
+  };
+
+  const [fetchStatus, dispatch] = useReducer(fetchReducer, IDLE);
+
+  const {error, data, isLoading} = fetchStatus;
+
+  const runFetch = useCallback(async (url, config) => {
+    dispatch({ type: FETCH_REDUCER_ACTIONS.PENDING })
+    try {
+      const res = await new Promise((resolve, reject) => {
+        setTimeout(async () => {
+          const response = await fetch(url, config);
+          if (response.ok) {
+            resolve(response);
+          } else {
+            reject(new Error(`Error fetching data: ${response.status} ${response.statusText}`));
+          }
+        }, 2000); // Retarder la résolution de 2 secondes
+      });
+
+      const data = await res.json();
+      dispatch({ type: FETCH_REDUCER_ACTIONS.RESOLVED, payload: data })
+    } catch (error) {
+      dispatch({ type: FETCH_REDUCER_ACTIONS.REJECTED, payload: error })
+    }
   }, []);
 
-  if(loading) return <Loader/>
+  // GitHub Repository - Exercise
+  useEffect(() => {
+    runFetch(getListOfUrlRepositoriesUrl(GITHUB_USERNAME))
+  }, []);
 
-  if(error) return <p className='text-center text-3xl'>{error.message}</p>
+  if (isLoading) return <Loader />
+
+  if (error) return <p className='text-center text-3xl'>{error.message}</p>
 
   return (
-      <SectionWrapper title="Projects">
-        <div className="flex flex-wrap justify-center gap-8">
-            {repositories.map((repo, index) => <Project key={index} {...repo} />)}
-        </div>
-      </SectionWrapper>
-    );
+    <SectionWrapper title="Projects">
+      <div className="flex flex-wrap justify-center gap-8">
+        {data && data.map((repo, index) => <Project key={index} {...repo} />)}
+      </div>
+    </SectionWrapper>
+  );
 };
