890d660951f43d3ec367c67ff35a6a1d78061946
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-04-29 15:34:19 +0200

    fix (canvas) = fix persisting value of color and size when component render

--- src/components/draw/Draw.jsx
+++ src/components/draw/Draw.jsx
@@ -11,33 +11,40 @@ export const DEFAULT_SIZE = 4;
 export const Draw = () => {
   //step 1 setup
   const canvas = useRef(null);
-//step 2 make default style
-useEffect(() => {
-  const context = canvas.current?.getContext('2d')
-  if(!context) return;
-  context.strokeStyle = DEFAULT_COLOR
-  context.lineWidth = DEFAULT_SIZE
-},[])
+  //step 2 make default style
 
- 
+  //step3 fix bug and refactor size and color selection and persisting
+  const canvasConfig = useRef({ color: DEFAULT_COLOR, size: DEFAULT_SIZE })
+
+  const handleConfigChange = () => {
+    const context = canvas.current.getContext("2d")
+    if (!context) return;
+    context.strokeStyle = canvasConfig.current.color
+    context.lineWidth = canvasConfig.current.size
+
+  }
+
+  useEffect(() => {
+    handleConfigChange()
+  }, [])
+
+
 
   return (
     <div className="flex flex-col gap-8">
-      <DrawCanvas canvas={canvas}/>
-      <DrawControl defaultColor={DEFAULT_COLOR} defaultSize={DEFAULT_SIZE} onChangeColor={(color)=>{
-        const context = canvas.current.getContext("2d")
-        if (!context) return
-
-        context.strokeStyle = color
+      <DrawCanvas canvas={canvas} />
+      <DrawControl defaultColor={DEFAULT_COLOR} defaultSize={DEFAULT_SIZE} onChangeColor={(color) => {
+        canvasConfig.current.color = color
+        handleConfigChange()
       }} onChangeSize={(size) => {
-        const context = canvas.current.getContext("2d")
-        if (!context) return
-
-        context.lineWidth = size
-      }}/>
+        canvasConfig.current.size = size
+        handleConfigChange()
+      }} />
       <div className="m-auto flex gap-4">
-        <Button onClick={() => {}}>Reset</Button>
-        <Button onClick={() => {}}>Save my drawing</Button>
+        <Button onClick={() => {
+          canvas.current.getContext("2d").clearRect(0, 0, canvas.current.width, canvas.current.height)
+        }}>Reset</Button>
+        <Button onClick={() => { }}>Save my drawing</Button>
       </div>
     </div>
   );
