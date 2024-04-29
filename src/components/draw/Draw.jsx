45207eb118a359ff2b2fa0084a236b11f55c531d
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-04-29 15:35:51 +0200

    feat (canvas) = step 4: save image of draw logic

--- src/components/draw/Draw.jsx
+++ src/components/draw/Draw.jsx
@@ -44,7 +44,13 @@ export const Draw = () => {
         <Button onClick={() => {
           canvas.current.getContext("2d").clearRect(0, 0, canvas.current.width, canvas.current.height)
         }}>Reset</Button>
-        <Button onClick={() => { }}>Save my drawing</Button>
+        <Button onClick={() => {
+          const data = canvas.current.toDataURL()
+          const link = document.createElement("a")
+          link.download = "image.png"
+          link.href = data
+          link.click()
+        }}>Save my drawing</Button>
       </div>
     </div>
   );
