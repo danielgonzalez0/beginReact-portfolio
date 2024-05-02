e80c332a1ebf108554539134cc9b1f84b95205b3
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-05-02 22:08:41 +0200

    feat(comments): get comments from notion and display them

--- src/components/comment/CommentSection.jsx
+++ src/components/comment/CommentSection.jsx
@@ -1,23 +1,26 @@
+
+import { useFetch } from '../../hooks/useFetch';
+import { commentsUrl } from '../../lib/api-url';
+import { Loader } from '../atom/Loader/Loader';
 import { SectionWrapper } from '../atom/SectionWrapper';
 import { Comment } from './Comment';
 import { CommentForm } from './CommentForm';
 
 export const CommentSection = () => {
-  // Commentaires - Exercise
-  const comments = [
-    {
-      username: 'John Doe',
-      comment: 'Hello World',
-      createdAt: new Date(1000),
-    },
-  ];
+
+  const {data, error, isLoading} = useFetch(commentsUrl)
+
+  if (isLoading) return <Loader/>
+
+  if (error) return <p className='text-center text-3xl'>{error.message}</p>
+
 
   return (
     <SectionWrapper title="On est à l'époque de FaceBook ?">
       <div className="flex flex-col items-center w-full max-w-2xl gap-8 m-auto ">
         <div className="grid justify-center w-full gap-4 grid-cols-auto-fill-200-300">
-          {/* Commentaires - Exercise */}
-          <Comment {...comments[0]} />
+          {data?.map((comment) => (
+          <Comment key={comment.id} {...comment} />))}
         </div>
         <CommentForm />
       </div>
